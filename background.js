a0a341379e9ee115ff797757b0dffa81dc6a7927
allahhh123 <eschneider778@gmail.com>
2024-06-14 22:16:32 -0700

    first commit

---
+++ background.js
@@ -0,0 +1,15 @@
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['content.js']
+    }, () => {
+      chrome.tabs.sendMessage(tab.id, { action: 'getHTML' }, (response) => {
+        if (response) {
+          console.log('HTML content:', response.html);
+        } else {
+          console.error('Failed to retrieve HTML content');
+        }
+      });
+    });
+  });
+  
